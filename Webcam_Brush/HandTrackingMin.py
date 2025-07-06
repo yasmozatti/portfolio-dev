954fe2243daf399d717a1a63628c7d3cf8cb0a88
yasmozatti <yasmimmozatti@gmail.com>
2025-07-06 17:00:31 -0300

    Adicionando o modulo

--- Webcam_Brush/HandTrackingMin.py
+++ Webcam_Brush/HandTrackingMin.py
@@ -19,6 +19,11 @@ while True:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
                 print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 8:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     cTime = time.time()
     fps = 1/(cTime-pTime)
