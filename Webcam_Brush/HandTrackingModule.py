bacf08b75bc1e49bc30423f788b85b95d0942bdd
yasmozatti <yasmimmozatti@gmail.com>
2025-07-07 15:40:21 -0300

    Adiçoes

--- Webcam_Brush/HandTrackingModule.py
+++ Webcam_Brush/HandTrackingModule.py
@@ -16,32 +16,38 @@ class handDetector():
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
         #print(results.multi_hand_landmarks)
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(img, handLms, 
+                                               self.mpHands.HAND_CONNECTIONS)
         return img
-                # for id, lm in enumerate(handLms.landmark):
-                #     print(id, lm)
-                #     h, w, c = img.shape
-                #     cx, cy = int(lm.x*w), int(lm.y*h)
-                #     print(id, cx, cy)
-                #     if id == 8:
-                #         cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-                # self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+    
+    def findPosition(self, img, handNo=0, draw= True):
 
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 8:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return lmList
+    
 
 def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(1)
     detector = handDetector()
-
     while True:
-        sucess, img = cap.read()
+        success, img = cap.read()
         img = detector.findHands(img)
 
         cTime = time.time()
